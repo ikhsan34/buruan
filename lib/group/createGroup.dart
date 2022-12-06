6d81f24bfa88b13a69de58f36e0009f2a03f4130
Ikhsan <ikhsanfirdaus74@gmail.com>
2022-12-06 23:04:09 +0700

    Changes to be committed: 	new file:   lib/group/createGroup.dart 	new file:   lib/group/group.dart 	new file:   lib/group/joinGroup.dart

---
+++ lib/group/createGroup.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class CreateGroup extends StatefulWidget {
+  const CreateGroup({super.key});
+
+  @override
+  State<CreateGroup> createState() => _CreateGroupState();
+}
+
+class _CreateGroupState extends State<CreateGroup> {
+
+  final api = 'http://ec2-13-250-57-227.ap-southeast-1.compute.amazonaws.com:5000';
+  bool isLoading = false;
+  final Future _prefs = SharedPreferences.getInstance();
+
+  createGroup(String name) async {
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final SharedPreferences prefs = await _prefs;
+    Map user = jsonDecode(prefs.getString('user') ?? '{}');
+
+    String token = 'Bearer ${prefs.getString('access_token')}';
+    Map<String, String> header = {'Authorization': token};
+
+    Map data = {
+      'user_id': user['id'],
+      'name': name
+    };
+
+    var response = await http.post(Uri.parse('$api/group'), body: data, headers: header);
+    
+    if(response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      print(result);
+      setState(() {
+        isLoading = false;
+        Navigator.of(context).pop();
+      });
+    }
+    print(response.statusCode);
+
+  }
+
+  final nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    const spinkit = SpinKitFoldingCube(
+      color: Color(0xff009688),
+      size: 50.0,
+    );
+    
+    return Scaffold(
+      backgroundColor: const Color(0xFFDEF5E5),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF9ED5C5),
+        title: const Text('Create Group'),
+      ),
+      body: isLoading ? spinkit : Column(
+        children: [
+          TextFormField(
+            controller: nameController,
+            keyboardType: TextInputType.name,
+            autofocus: false,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'name',
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF009688)),
+              onPressed: () {
+                //Navigator.of(context).pushNamed(HomePage.tag);
+                setState(() {
+                  isLoading = true;
+                });
+                createGroup(nameController.text);
+              },
+              child: const Text('Register'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/group/group.dart
@@ -0,0 +1,133 @@
+import 'package:buruan/group/createGroup.dart';
+import 'package:buruan/group/joinGroup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Group extends StatefulWidget {
+  static String tag = 'Group-page';
+
+  const Group({super.key});
+  @override
+  _GroupState createState() => _GroupState();
+}
+
+class _GroupState extends State<Group> {
+
+  final Future _prefs = SharedPreferences.getInstance();
+  bool isLoading = true;
+  List group = [];
+
+  void getGroup() async {
+
+    final SharedPreferences prefs = await _prefs;
+    Map user = jsonDecode(prefs.getString('user') ?? '{}');
+    const api = 'http://ec2-13-250-57-227.ap-southeast-1.compute.amazonaws.com:5000';
+
+    String token = 'Bearer ${prefs.getString('access_token')}';
+    Map<String, String> header = {'Authorization': token};
+
+    var response = await http.get(Uri.parse("$api/group/user/${user['id']}"), headers: header);
+    var result = jsonDecode(response.body)['group'];
+
+    if(result != null) {
+      for (var item in result) { 
+        await http.get(Uri.parse("$api/group/${item['group_id']}"), headers: header).then((value)  {
+          var response = jsonDecode(value.body);
+          item['group_name'] = response['group']['name'];
+        });
+      }
+    }
+    
+    if(mounted) {
+      setState(() {
+        group = result ?? [];
+        isLoading = false;
+      });
+    }
+
+  }
+
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getGroup();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+
+    getGroup();
+
+    final int crossAxisCount = group.length < 2 ? 1 : 2;
+
+    const spinkit = SpinKitFoldingCube(
+      color: Color(0xff009688),
+      size: 50.0,
+    );
+
+    var gridView = GridView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 100),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        mainAxisSpacing: 30,
+        crossAxisSpacing: 30,
+        crossAxisCount: crossAxisCount,
+        childAspectRatio: 3 / 1
+      ),
+      itemCount: group.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Colors.white,
+          child: Center(
+            child: Text(group[index]['group_name']),
+          )
+        );
+      },
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Group"),
+        backgroundColor: const Color(0xFF9ED5C5),
+      ),
+      backgroundColor: const Color(0xFFDEF5E5),
+      floatingActionButton: Wrap(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton.extended(
+              heroTag: 'create-group',
+              backgroundColor: const Color(0xFF009688),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: ((context) => const CreateGroup())));
+              },
+              label: const Text('Create Group'),
+            ),
+          ), //button first
+
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton.extended(
+              heroTag: 'join-group',
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: ((context) => const JoinGroup())));
+              },
+              backgroundColor: const Color(0xFF009688),
+              label: const Text('Join Group'),
+            ),
+          ), // button second
+        ],
+      ),
+      body: isLoading ? spinkit : group.isEmpty ? 
+      const Center(
+        child: Text('You dont have any group'),
+      )
+      : gridView
+    );
+  }
+}
