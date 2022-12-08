ef10d654fce4c107b33e0bfb3e1a6c1f0b3b04be
fakhrimuh <muhammad.fakhri1234@gmail.com>
2022-12-08 14:45:54 +0700

    add row group

--- lib/group.dart
+++ lib/group.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:buruan/groupDetail.dart';
 
 class Group extends StatefulWidget {
   static String tag = 'Group-page';
@@ -13,21 +14,25 @@ class _GroupState extends State<Group> {
   Widget build(BuildContext context) {
     final GroupCard = Padding(
       padding: const EdgeInsets.symmetric(vertical: 0),
-      child: Card(
-        child: InkWell(
-          splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            // Navigator.of(context).pushNamed(ReminderDetail.tag);
-          },
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                // leading: Icon(Icons.album),
-                title: Text('Group Name'),
-                subtitle: Text('Member: 12'),
-              )
-            ],
+      child: SizedBox(
+        width: 180,
+        height: 80,
+        child: Card(
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {
+              Navigator.of(context).pushNamed(GroupDetail.tag);
+            },
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                ListTile(
+                  // leading: Icon(Icons.album),
+                  title: Text('Group Name'),
+                  subtitle: Text('Member: 12'),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -45,24 +50,65 @@ class _GroupState extends State<Group> {
           Container(
             margin: EdgeInsets.all(10),
             child: FloatingActionButton.extended(
+              heroTag: "btn 1",
               backgroundColor: Color(0xFF009688),
               onPressed: () {},
               label: const Text('Create Group'),
             ),
           ), //button first
 
+// Join Group Button
           Container(
-            margin: EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
             child: FloatingActionButton.extended(
-              onPressed: () {},
-              backgroundColor: Color(0xFF009688),
+              heroTag: "btn 2",
+              onPressed: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                        title: const Text("Group Code"),
+                        content: TextFormField(
+                            keyboardType: TextInputType.text,
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  20.0, 10.0, 20.0, 10.0),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              enabledBorder: const OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Color(0xFF009688))),
+                            )),
+                        actions: <Widget>[
+                          Center(
+                              child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                side: BorderSide(
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                                backgroundColor: const Color(0xFF009688)),
+                            child: const Text("Join"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ))
+                        ],
+                      )),
+              backgroundColor: const Color(0xFF009688),
               label: const Text('Join Group'),
             ),
           ), // button second
         ],
       ),
       body: Center(
-        child: GroupCard,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GroupCard,
+            GroupCard,
+          ],
+        ),
       ),
     );
   }
